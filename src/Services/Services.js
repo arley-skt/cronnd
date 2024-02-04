d1f0c3ef5c6ae9eaf934c97507339a189e187309
arley.ferreira <arley.ferreira@itlean.com.br>
2024-02-04 15:30:52 -0300

    rota cronad

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -14,10 +14,14 @@ class Services {
   // }
 
   async criaRegistro(dadosDoRegistro) {
+    const users = await sequelize.query("SELECT*FROM `pessoas´ ORDER BY id DESC LIMIT ´1;´", { type: QueryTypes.SELECT });
+    console.log(users);
     return dataSource[this.model].create(dadosDoRegistro);
   }
 
     async criaRegistrocron(dadosDoRegistro) {
+      var Id = dataSource[this.model]
+      var obj ='{"id":9,"nome":"Joao Estudante","email":"joao@email.com","cpf":"63058133022","ativo":true,"role":"engenheiro","createdAt":"2024-02-04T04:20:00.794Z","updatedAt":"2024-02-04T18:01:08.392Z"}'
     return dataSource[this.model].create(dadosDoRegistro);
   }
   async atualizaRegistro(dadosAtualizados, id) {
